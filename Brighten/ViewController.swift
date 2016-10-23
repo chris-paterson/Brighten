67dbcbf61f04e3bc415863684f316ee18faa066a
chris-paterson <cjpaterson.3@gmail.com>
2016-10-23 08:51:24 +0100

    Increase brightness on tap

--- Brighten/ViewController.swift
+++ Brighten/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var _greyscaleLevel: Float = 0
+    var greyscaleLevel: Float {
+        get {
+            return _greyscaleLevel
+        }
+        set {
+            _greyscaleLevel = newValue
+            let convertedGreyscale = CGFloat(_greyscaleLevel)
+            self.view.backgroundColor = UIColor(white: convertedGreyscale, alpha: 1.0)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor(white: CGFloat(_greyscaleLevel), alpha: 1.0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func screenTap(_ sender: AnyObject) {
+        incrementBackgroundColour()
+    }
+    
+    func incrementBackgroundColour() {
+        greyscaleLevel += 0.1
+    }
+    
+    
 }
 
