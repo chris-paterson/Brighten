4bd7c68b3e3e84a0529858703465005675cc94b5
chris-paterson <cjpaterson.3@gmail.com>
2016-10-23 08:23:22 +0100

    Initial Commit

---
+++ Brighten/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brighten
+//
+//  Created by Christopher Paterson on 23/10/2016.
+//  Copyright © 2016 Christopher Paterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
